a76ecb3a991955dfc222cbea93174f15513b092f
rmdhnaw <ariwahyu1.aw@gmail.com>
2023-07-04 23:15:21 +0700

    menambahkan latihan1.php

---
+++ kuliah/pertemuan10/latihan1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+  <table border="1" cellpadding="10" cellspacing="0"></table>
+  <tr>
+    <th>#</th>
+    <th>Gambar</th>
+    <th>NRP</th>
+    <th>Nama</th>
+    <th>Email</th>
+    <th>Jurusan</th>
+    <th>Aksi</th>
+
+  </tr>
+</body>
+
+</html>
